753cbf20dc9e8244308f526a935859468017b758
Satyam Mishra <satyammishra9050@gmail.com>
2025-03-01 21:44:48 +0530

    added new pdf extraction method code

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from utils.preprocessing import get_gemini_api_key, get_gemini_llm
-from modules.pdf_processor import extract_text_from_pdf, clean_text
+from modules.pdf_processor import get_pdf
 
 GEMINI_API = get_gemini_api_key()
 engine = get_gemini_llm(GEMINI_API)
 # print(engine.invoke("Hello, what is your name?").content)
 
-#1 PDF Extraction
-raw_text=extract_text_from_pdf("ace.pdf")
-cleaned_text=clean_text(raw_text)
-print(cleaned_text[:500]) #first 500 characters
+# 1 PDF Extraction
+# raw_text = extract_text_from_pdf("ace.pdf")
+# cleaned_text = clean_text(raw_text)
+# print(cleaned_text[:500])  # first 500 characters
+
+data = get_pdf("cache_files/pdfs/ace.pdf")
+for element in data:
+    print(element)
+    print("\n")
