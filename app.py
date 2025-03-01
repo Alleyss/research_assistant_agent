b97ed7352f3d96e9a0413b19c483157de51a097b
Alleyss <vsaipranav2004@gmail.com>
2025-03-02 00:41:34 +0530

    working embeddings

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from utils.preprocessing import get_gemini_api_key, get_gemini_llm
 from modules.pdf_processor import get_pdf_elements
-
+from modules.embeddings import generate_embeddings
 GEMINI_API = get_gemini_api_key()
 engine = get_gemini_llm(GEMINI_API)
 # print(engine.invoke("Hello, what is your name?").content)
@@ -11,6 +11,12 @@ engine = get_gemini_llm(GEMINI_API)
 # print(cleaned_text[:500])  # first 500 characters
 
 data = get_pdf_elements("cache_files/pdfs/ace.pdf")
-for element in data:
-    print(element)
-    print("\n")
+# for element in data:
+#     print(element)
+#     print("\n")
+
+embeddings=generate_embeddings(data)
+# for em in embeddings:
+#     print(em)
+#     print("\n")
+
