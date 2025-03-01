ab3b38014b0a8ebcb13de2fb6ba0ae62f4194a24
Alleyss <vsaipranav2004@gmail.com>
2025-03-01 21:09:34 +0530

    called extract text in app.py and updated requirements

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from utils.preprocessing import get_gemini_api_key, get_gemini_llm
-
+from modules.pdf_processor import extract_text_from_pdf, clean_text
 
 GEMINI_API = get_gemini_api_key()
 engine = get_gemini_llm(GEMINI_API)
-print(engine.invoke("Hello, what is your name?").content)
+# print(engine.invoke("Hello, what is your name?").content)
+
+#1 PDF Extraction
+raw_text=extract_text_from_pdf("ace.pdf")
+cleaned_text=clean_text(raw_text)
+print(cleaned_text[:500]) #first 500 characters
